65040dc01ff0ff90f9d115f04b412c63a1e9249a
sorahza <sorahza@gmail.com>
2023-07-10 17:10:43 -0400

    initial commit

---
+++ WindowsFormUtility.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPUWindowsFormFramework
+{
+    public class WindowsFormUtility
+    {
+        public static void SetListBinding(ComboBox lst, DataTable dtsource, string tablename, DataTable dttarget)
+        {
+            lst.DataSource = dtsource;
+            lst.DisplayMember = lst.Name.Substring(3);
+            lst.ValueMember = tablename + "Id";
+            lst.DataBindings.Add("SelectedValue", dttarget, lst.ValueMember, true, DataSourceUpdateMode.OnPropertyChanged);
+        }
+        public static void SetControlBinding(Control ctrl, DataTable dt)
+        {
+            string propertyname = "";
+            string controlname = ctrl.Name.ToLower();
+            string columnname = controlname.Substring(3);
+            if (controlname.StartsWith("txt") || controlname.StartsWith("lbl"))
+            {
+                propertyname = "Text";
+
+            }
+            else if (controlname.StartsWith("dtp"))
+            {
+                propertyname = "Value";
+            }
+            if (columnname != "" && propertyname != "")
+            {
+                ctrl.DataBindings.Add(propertyname, dt, columnname, true, DataSourceUpdateMode.OnPropertyChanged);
+
+            }
+        }
+    }
+}
